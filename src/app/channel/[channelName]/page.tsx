673b9abd3b593cfd15b675f2d0051bd6352adc83
ayodyabanuka <ayodyabanukafernando@gmail.com>
2024-04-01 14:36:51 +0530

    update

--- src/app/channel/[channelName]/page.tsx
+++ src/app/channel/[channelName]/page.tsx
@@ -5,17 +5,14 @@ import { useEffect } from "react"
 
 
 export default function Page({ params }: { params: { channelName: string } }) {
-       useEffect(() => {
-              console.log(process.env.PUBLIC_AGORA_APP_ID!);
 
-       }, [])
        return (
 
               <main className="flex w-full flex-col">
                      <p className="absolute z-10 mt-2 ml-12 text-2xl font-bold text-gray-900">
                             {params.channelName!}
                      </p>
-                     <Call appId={'5ad9d828d4ab48c08c08d195fee669c5'} channelName={params.channelName}></Call>
+                     <Call appId={process.env.APP_ID!} channelName={params.channelName}></Call>
               </main>
        )
 }
